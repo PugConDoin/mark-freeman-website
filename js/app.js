9fd212faf1e4736348a6e8699edf9a9799825223
Mark Freeman <dorsettri@gmail.com>
2021-01-23 22:18:05 +0000

    add hue filter and scroll button

--- js/app.js
+++ js/app.js
@@ -7,3 +7,20 @@ const navButton = document.querySelector('button[aria-expanded]');
 
     navButton.addEventListener('click', toggleNav);
 
+function getYear() {
+  let currentYear = new Date().getFullYear();
+  document.querySelector('.copy').textContent = `Mark Freeman ${ currentYear }`;
+}
+getYear();
+
+const scroller = document.getElementById('scroller');
+const nav = document.querySelector('nav');
+// Add a handler for the 'click' event by providing a callback function.
+// Whenever the element is clicked, a pop-up with "Element clicked!" will
+// appear.
+
+scroller.addEventListener('click', function() {
+  nav.scrollIntoView({
+    behavior: 'smooth'
+  })
+})
